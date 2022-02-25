6c4f09151ae032166dd716160b4d40fd3a4bb7cd
AxelZublena <zubl0001@hz.nl>
2022-02-25 19:18:24 +0100

    Server now accepts 2 files (formdata)

--- src/pdf.ts
+++ src/pdf.ts
@@ -19,3 +19,28 @@ export async function modifyPdf(input: string) {
 	const pdfBytes = await pdfDoc.save()
 	return pdfBytes;
 }
+
+export async function appendPdf(file1: string, file2: string) {
+	const pdfDoc1 = await PDFDocument.load(file1)
+	const pdfDoc2 = await PDFDocument.load(file2)
+	// const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica)
+
+	// const pages1 = pdfDoc1.getPages()
+	// const pages2 = pdfDoc2.getPages()
+	const copiedPages = await pdfDoc1.copyPages(pdfDoc2, [0])
+	pdfDoc1.insertPage(1, copiedPages[0])
+	// pages2.forEach(page => pdfDoc1.addPage(page));
+	// const firstPage = pages1[0]
+	// const { width, height } = firstPage.getSize()
+	// firstPage.drawText('This text was added with JavaScript!', {
+	// 	x: 5,
+	// 	y: height / 2 + 300,
+	// 	size: 60,
+	// 	font: helveticaFont,
+	// 	color: rgb(0.95, 0.1, 0.1),
+	// 	rotate: degrees(-45),
+	// })
+
+	const pdfBytes = await pdfDoc1.save()
+	return pdfBytes;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,31 @@
-import http, { RequestListener } from "http";
-import { modifyPdf } from "./pdf";
+import http, { RequestListener, ServerResponse } from "http";
+import { appendPdf } from "./pdf";
 
 import { Stream } from "stream";
+import * as fs from "fs";
+import multiparty from "multiparty";
 
 const requestListener: RequestListener = async function(req: any, res: any) {
-	const chunks: Uint8Array[] = [];
-	req.on('data', (chunk: any) => chunks.push(chunk));
-	req.on('end', async () => {
-		const buffer: Buffer = Buffer.concat(chunks);
-		const data: string = Buffer.from(buffer).toString("base64")
+	let form = new multiparty.Form();
 
-		const outFile = await modifyPdf(data);
+	form.parse(req, async function(err, fields, files) {
+		const fileInfo1 = files.file1[0];
+		const fileInfo2 = files.file2[0];
+		const file1 = fs.readFileSync(fileInfo1.path, "base64");
+		const file2 = fs.readFileSync(fileInfo2.path, "base64");
+		const outFile = await appendPdf(file1, file2);
 		res.writeHead(200, {
+			'content-type': 'text/plain',
 			'Content-Type': 'application/pdf',
 			'Content-Length': outFile.length,
 			'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
 			'Access-Control-Max-Age': 2592000 // 30 days
 		});
-
 		let fileStream = new Stream.PassThrough();
 		fileStream.end(outFile);
 		fileStream.pipe(res);
-	})
+	});
 }
 
 const server = http.createServer(requestListener);
